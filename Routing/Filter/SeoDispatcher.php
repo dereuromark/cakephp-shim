3c642d072cb2682a5396e67aceb002675d6065cd
Mark Scherer <euromark@web.de>
2015-02-04 01:25:48 +0100

    Cleanup

--- Routing/Filter/SeoDispatcher.php
+++ Routing/Filter/SeoDispatcher.php
@@ -10,33 +10,39 @@ App::uses('Router', 'Routing');
  */
 class SeoDispatcher extends DispatcherFilter {
 
-/**
- * Default priority for all methods in this filter
- * This filter should run after the request gets parsed by router
- *
- * @var int
- */
+	/**
+	 * Default priority for all methods in this filter
+	 * This filter should run after the request gets parsed by router
+	 *
+	 * @var int
+	 */
 	public $priority = 11;
 
-/**
- * Checks if a valid URL has been requested.
- *
- * E.g. valid:
- * - /admin/plugin_name/controller_name/action_name
- *
- * All invalid, but unfortunately working wihtout this dispatcher:
- * - /admin/pluginName/controllerName/actionName
- * - /ControllerName/Action_name
- * - etc
- *
- * @param CakeEvent $event containing the request and response object
- * @return void|Response
- * @throws NotFoundException When asset not found
- */
+	/**
+	 * The request fields to process
+	 *
+	 * @var array
+	 */
+	public $fields = ['prefix', 'plugin', 'controller', 'action'];
+
+	/**
+	 * Checks if a valid URL has been requested.
+	 *
+	 * E.g. valid:
+	 * - /admin/plugin_name/controller_name/action_name
+	 *
+	 * All invalid, but unfortunately working wihtout this dispatcher:
+	 * - /admin/pluginName/controllerName/actionName
+	 * - /ControllerName/Action_name
+	 * - etc
+	 *
+	 * @param CakeEvent $event containing the request and response object
+	 * @return void|Response
+	 * @throws NotFoundException When URL invalid
+	 */
 	public function beforeDispatch(CakeEvent $event) {
 		$request = $event->data['request'];
-		$fields = array('prefix', 'plugin', 'controller', 'action');
-		$fields = array_merge($fields);
+		$fields = $this->fields;
 
 		// Convention is snake_cased URL pieces
 		$urlPieces = array();
@@ -57,6 +63,7 @@ class SeoDispatcher extends DispatcherFilter {
 		if ($ok) {
 			return;
 		}
+
 		// For debugging without this dispatcher - only for debug mode
 		if (Configure::read('debug') && $request->query('skip_seo')) {
 			return;
