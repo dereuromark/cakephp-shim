b4f6e1371a8407393ae87646c3998e019760fd45
Mark Scherer <euromark@web.de>
2015-04-08 21:50:28 +0200

    Allow 2.x way of setting default data to the view.

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -3,6 +3,8 @@ namespace Shim\Controller;
 
 use Cake\Controller\Controller as CakeController;
 use Cake\Core\Configure;
+use Cake\Event\Event;
+use Cake\ORM\Entity;
 
 /**
  * DRY Controller stuff
@@ -34,6 +36,19 @@ class Controller extends CakeController {
 			$this->paginate += $defaultSettings;
 		}
 		return parent::paginate($object);
+	}
+
+	/**
+	 * @param Event $event
+	 * @return void
+	 */
+	public function beforeRender(Event $event) {
+		parent::beforeRender($event);
+
+		// Automatically shim $this->request->data = $this->Model->find() etc which used to be of type array
+		if (!empty($this->request->data) && $this->request->data instanceof Entity) {
+			$this->request->data = $this->request->data->toArray();
+		}
 	}
 
 }
