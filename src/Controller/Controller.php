4e8a13ae8c2d0e82bd3de61e992f3c57e107b59e
dereuromark <mark.scherer@spryker.com>
2017-12-17 15:37:40 +0100

    Add basic uses shim

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,7 +4,10 @@ namespace Shim\Controller;
 use Cake\Controller\Controller as CoreController;
 use Cake\Core\Configure;
 use Cake\Event\Event;
+use Cake\Http\Response;
+use Cake\Http\ServerRequest;
 use Cake\ORM\Entity;
+use Cake\Utility\Inflector;
 use Exception;
 
 /**
@@ -13,6 +16,31 @@ use Exception;
  * @property \Shim\Controller\Component\SessionComponent $Session
  */
 class Controller extends CoreController {
+
+	/**
+	 * Sets a number of properties based on conventions if they are empty. To override the
+	 * conventions CakePHP uses you can define properties in your class declaration.
+	 *
+	 * @param \Cake\Http\ServerRequest|null $request Request object for this controller. Can be null for testing,
+	 *   but expect that features that use the request parameters will not work.
+	 * @param \Cake\Http\Response|null $response Response object for this controller.
+	 * @param string|null $name Override the name useful in testing when using mocks.
+	 * @param \Cake\Event\EventManager|null $eventManager The event manager. Defaults to a new instance.
+	 * @param \Cake\Controller\ComponentRegistry|null $components The component registry. Defaults to a new instance.
+	 */
+	public function __construct(ServerRequest $request = null, Response $response = null, $name = null, $eventManager = null, $components = null)
+	{
+		if (!isset($this->modelClass) && !empty($this->uses)) {
+			$uses = (array)$this->uses;
+			$model = array_shift($uses);
+			$modelClass = Inflector::pluralize($model);
+			$this->modelClass = $modelClass;
+
+			trigger_error('Use $modelClass instead of $uses property.', E_USER_DEPRECATED);
+		}
+
+		parent::__construct($request, $response, $name, $eventManager, $components);
+	}
 
 	/**
 	 * Add headers for IE8 etc to fix caching issues in those stupid browsers
