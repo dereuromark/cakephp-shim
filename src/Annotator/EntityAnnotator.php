ddb74fd0737900304addee5ffc63382560ac82be
mscherer <mark.scherer@spryker.com>
2019-05-06 00:38:39 +0200

    Add Entity getOrFail() annotator.

---
+++ src/Annotator/EntityAnnotator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Shim\Annotator;
+
+use Cake\Utility\Inflector;
+use IdeHelper\Annotation\MethodAnnotation;
+use IdeHelper\Annotator\EntityAnnotator as IdeHelperEntityAnnotator;
+use IdeHelper\View\Helper\DocBlockHelper;
+
+class EntityAnnotator extends IdeHelperEntityAnnotator {
+
+	/**
+	 * @param array $propertyHintMap
+	 * @param \IdeHelper\View\Helper\DocBlockHelper $helper
+	 *
+	 * @return \IdeHelper\Annotation\AbstractAnnotation[]
+	 * @throws \RuntimeException
+	 */
+	protected function buildAnnotations(array $propertyHintMap, DocBlockHelper $helper) {
+		$map = parent::buildAnnotations($propertyHintMap, $helper);
+
+		$class = $this->getConfig('class');
+		if (!$class || !method_exists($class, 'getOrFail')) {
+			return $map;
+		}
+
+		foreach ($propertyHintMap as $field => $type) {
+			$method = 'get' . Inflector::camelize($field) . 'OrFail()';
+			if (strpos($type, '|null') !== false) {
+				$type = str_replace('|null', '', $type);
+			}
+
+			$map[] = new MethodAnnotation($type, $method);
+		}
+
+		return $map;
+	}
+
+}
