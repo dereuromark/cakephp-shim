c5809bbe9334b1f692f5d5bf2ce21c1a0ee62bb1
mscherer <mark.scherer@spryker.com>
2019-05-08 01:57:24 +0200

    Add more tests.

--- tests/TestCase/Model/Entity/EntityReadTest.php
+++ tests/TestCase/Model/Entity/EntityReadTest.php
@@ -36,8 +36,44 @@ class EntityReadTest extends TestCase {
 		];
 
 		$result = $entity->read('tags.2.name');
-
 		$this->assertSame('foo', $result);
+
+		$result = $entity->read('tags.2.name_not_exists');
+		$this->assertNull($result);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testReadDefaultValue() {
+		$entity = new TestEntity();
+		$entity->foo_bar = 'Foo Bar';
+
+		$result = $entity->read('foo_bar', false);
+		$expected = 'Foo Bar';
+		$this->assertSame($expected, $result);
+
+		$result = $entity->read('invalid', false);
+		$this->assertFalse($result);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function testReadDeepDefaultValue() {
+		$entity = new TestEntity();
+
+		$entity->tags = [
+			new Entity(),
+			new Entity(),
+			new Entity(['name' => new Entity(['subname' => 'foo'])]),
+		];
+
+		$result = $entity->read('tags.2.name.subname', false);
+		$this->assertSame('foo', $result);
+
+		$result = $entity->read('tags.2.name.subname_not_exists', false);
+		$this->assertFalse($result);
 	}
 
 }
