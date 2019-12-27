1b5d0060ed5f5adc54477e351a95f4631303ae3c
Mark Sch <dereuromark@users.noreply.github.com>
2019-12-28 00:44:37 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -84,16 +84,13 @@ if (getenv('DB_DSN')) {
 
 if (!getenv('DB_CLASS')) {
 	putenv('DB_CLASS=Cake\Database\Driver\Sqlite');
-	putenv('DB_DSN=sqlite::memory:');
+	putenv('DB_DSN=sqlite:///:memory:');
 }
 
 Cake\Datasource\ConnectionManager::setConfig('test', [
 	'className' => 'Cake\Database\Connection',
 	'driver' => getenv('DB_CLASS'),
 	'dsn' => getenv('DB_DSN'),
-	//'database' => getenv('DB_DATABASE'),
-	//'username' => getenv('DB_USERNAME'),
-	//'password' => getenv('DB_PASSWORD'),
 	'timezone' => 'UTC',
 	'quoteIdentifiers' => true,
 	'cacheMetadata' => true,
