f9385a68dc20d010555600a64f5f6ea9297b906d
mscherer <mark.scherer@spryker.com>
2020-04-19 19:05:24 +0200

    Add command base class

---
+++ src/Command/Command.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Shim\Command;
+
+use Cake\Command\Command as CakeCommand;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+
+/**
+ * Convenience wrapper to more easily access args and io in submethods.
+ *
+ * Your execute() must call `parent::execute($args, $io);` as first line.
+ */
+class Command extends CakeCommand {
+
+	/**
+	 * @var \Cake\Console\Arguments
+	 */
+	protected $args;
+
+	/**
+	 * @var \Cake\Console\ConsoleIo
+	 */
+	protected $io;
+
+	/**
+	 * @param \Cake\Console\Arguments $args
+	 * @param \Cake\Console\ConsoleIo $io
+	 *
+	 * @return int|void|null
+	 */
+	public function execute(Arguments $args, ConsoleIo $io)
+	{
+		$this->args = $args;
+		$this->io = $io;
+	}
+
+}
