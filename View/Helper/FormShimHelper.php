a859974c7d93441c74387c72ef56d2af0b0e6760
Ae. Bogdan <ba.ro@dinnerbooking.com>
2020-10-08 10:28:06 +0300

    Update FormShimHelper.php

--- View/Helper/FormShimHelper.php
+++ View/Helper/FormShimHelper.php
@@ -204,7 +204,9 @@ class FormShimHelper extends FormHelper {
 	public function control($fieldName, array $options = []) {
 		$optionKeys = array_keys($options);
 		$this->_checkDeprecatedInputOptions($optionKeys);
-		$options['div'] = false;
+		if (!empty(Configure::read('Shim.warnAboutFormInputs'))) {
+			$options['div'] = false;
+		}
 		return parent::input($fieldName, $options);
 	}
 
