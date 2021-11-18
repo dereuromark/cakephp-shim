b08cfd934cf3ecdf253d1c3ec1944fc037885b15
mscherer <mark.scherer@spryker.com>
2021-11-18 03:39:08 +0100

    Shim view vars helpers setting.

--- src/View/ViewVarsTrait.php
+++ src/View/ViewVarsTrait.php
@@ -7,19 +7,19 @@ use Cake\View\ViewBuilder;
 /**
  * Provides own ViewBuilder with 5.x way of setting and handling helpers.
  */
-trait ViewVarsTrait
-{
+trait ViewVarsTrait {
+
     /**
      * Get the view builder being used.
      *
      * @return \Cake\View\ViewBuilder
      */
-    public function viewBuilder(): ViewBuilder
-    {
+    public function viewBuilder(): ViewBuilder {
         if (!isset($this->_viewBuilder)) {
             $this->_viewBuilder = new ShimViewBuilder();
         }
 
         return $this->_viewBuilder;
     }
+
 }
