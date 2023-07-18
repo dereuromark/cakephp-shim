787b2c025621b3ef66d9c62e589ac79beeacaa3c
mscherer <euromark@web.de>
2023-07-18 14:25:58 +0200

    Add removed const shims

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -16,3 +16,25 @@ Configure::write('Shim.deprecations', true);
 */
 
 Configure::write('Shim.deprecations', true);
+
+if (!defined('SECOND')) {
+	define('SECOND', 1);
+}
+if (!defined('MINUTE')) {
+	define('MINUTE', 60);
+}
+if (!defined('HOUR')) {
+	define('HOUR', 3600);
+}
+if (!defined('DAY')) {
+	define('DAY', 86400);
+}
+if (!defined('WEEK')) {
+	define('WEEK', 604800);
+}
+if (!defined('MONTH')) {
+	define('MONTH', 2592000);
+}
+if (!defined('YEAR')) {
+	define('YEAR', 31536000);
+}
