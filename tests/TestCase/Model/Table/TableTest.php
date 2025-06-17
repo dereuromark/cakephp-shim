bc27c0a09dcb6017c737a79e90ab4b4eff34b518
mscherer <dereuromark@web.de>
2025-06-17 02:32:10 +0200

    Revert find list shim, too many side effects.

--- tests/TestCase/Model/Table/TableTest.php
+++ tests/TestCase/Model/Table/TableTest.php
@@ -413,30 +413,4 @@ class TableTest extends TestCase {
 		$this->Wheels->save($wheel, ['strict' => true]);
 	}
 
-	/**
-	 * @return void
-	 */
-	public function testFindList(): void {
-		$result = $this->Posts->find('list', fields: ['title', 'body'])->toArray();
-		$expected = [
-			'First Post' => 'First Post Body',
-			'Second Post' => 'Second Post Body',
-			'Third Post' => 'Third Post Body',
-		];
-		$this->assertEquals($expected, $result);
-	}
-
-	/**
-	 * @return void
-	 */
-	public function testFindListSingleField(): void {
-		$result = $this->Posts->find('list', fields: ['id'])->toArray();
-		$expected = [
-			1 => 1,
-			2 => 2,
-			3 => 3,
-		];
-		$this->assertEquals($expected, $result);
-	}
-
 }
